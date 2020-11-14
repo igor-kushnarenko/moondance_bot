433f1cfaa022d87a2ed56591faa89574b7c6d772
igor <anapa.igor@yandex.ru>
2020-11-14 09:46:42 +0300

    исправил модуль add_user.py для добавления пользователей в базу. Теперь он пишется как set

--- add_user.py
+++ add_user.py
@@ -1,16 +1,18 @@
 import pickle
 
+file_data = 'user_base.pickle'
+
 
 def add_user(id):
-    user_id = set()
-    with open('user_base.pickle', 'rb') as f:
-        user_id.add(pickle.load(f))
-        print(user_id)
+    with open(file_data, 'rb') as f:
+        user_id = pickle.load(f)
+        print(f'Массив загружен: {user_id}')
         if id not in user_id:
-            print(f'Пользователя {id} нет в базе.')
+            print(f'Пользователя {id} нет в базе')
             user_id.add(id)
-            with open('user_base.pickle', 'ab') as f:
+            print(f'Пользователь {id} добавлен в базу')
+            with open(file_data, 'wb') as f:
                 pickle.dump(user_id, f)
-                print(f'Пользователь {id} добавлен в базу.')
+                print(f'База сохранена')
         else:
-            print(f'Пользователь {id} уже давно в базе.')
+            print('Пользователь там!')
